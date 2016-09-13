040899c9786bba8f76825d17741c17e4ab936083
Min RK <benjaminrk@gmail.com>
2016-09-13 12:09:34 +0200

    cleanup `-m notebook.auth` a bit
    - exit on KeyboardInterrupt
    - add doctoring, description
    - make command mandatory for better error if not given

--- notebook/auth/__main__.py
+++ notebook/auth/__main__.py
@@ -1,14 +1,21 @@
-"""Generate a hashed password for use in Jupyter Notebook"""
+"""Generate a hashed password for use in Jupyter Notebook.
+
+This command stores the generated password in your jupyter_notebook_config.json.
+
+"""
 
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
-from notebook.auth import passwd
+import argparse
 from getpass import getpass
+import sys
+
 from traitlets.config.manager import BaseJSONConfigManager
 from jupyter_core.paths import jupyter_config_dir
-import argparse
-import sys
+
+from . import passwd
+
 
 def set_password(args):
     password = args.password
@@ -17,8 +24,8 @@ def set_password(args):
         password_repeat = getpass("" if args.quiet else "Repeat password:  ")
         if password1 != password_repeat:
             print("Passwords do not match, try again")
-        elif len(password1) < 4:
-            print("Please provide at least 4 characters")
+        elif not pasword1:
+            print("Please provide a password")
         else:
             password = password1
 
@@ -32,16 +39,22 @@ def set_password(args):
     if not args.quiet:
         print("password stored in config dir: %s" % jupyter_config_dir())
 
+
 def main(argv):
-    parser = argparse.ArgumentParser(argv[0])
-    subparsers = parser.add_subparsers()
+    parser = argparse.ArgumentParser('notebook.auth', description=__doc__)
+    subparsers = parser.add_subparsers(dest='command')
+    subparsers.required = True
     parser_password = subparsers.add_parser('password', help='sets a password for your notebook server')
     parser_password.add_argument("password", help="password to set, if not given, a password will be queried for (NOTE: this may not be safe)",
             nargs="?")
     parser_password.add_argument("--quiet", help="suppress messages", action="store_true")
     parser_password.set_defaults(function=set_password)
     args = parser.parse_args(argv[1:])
-    args.function(args)
+    try:
+        args.function(args)
+    except KeyboardInterrupt:
+        print('') # newline
+        sys.exit(1)
 
 if __name__ == "__main__":
     main(sys.argv)
